4144dba9ff7e1ffb33566e4b11e93472b3a3e120
dlturner29@icloud.com <turnerde@udel.edu>
2024-01-30 20:31:11 -0500

    switched category name back to original to see if issue is with some sort of reference

--- Script.js
+++ Script.js
@@ -3,9 +3,9 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 	var API = new APIConstructor();
 		  return stiatExtension({
 		  category : { 
-		    name : 'Fat people', //Will appear in the data.
+		    name : 'Black people', //Will appear in the data.
 		    title : {
-		      media : {word : 'Fat people'}, //Name of the category presented in the task.
+		      media : {word : 'Black people'}, //Name of the category presented in the task.
 		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
