9e305f5fbb3106ca62010da167d650fca9fd8dbf
dlturner29@icloud.com <turnerde@udel.edu>
2024-02-06 22:26:52 -0500

    trying a different example script because the first example I used was only single target

---
+++ Old_Script.js
@@ -0,0 +1,87 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+	
+	var API = new APIConstructor();
+		  return stiatExtension({
+		  category : { 
+		    name : 'Fat People', //Will appear in the data.
+		    title : {
+		      media : {word : 'Fat People'}, //Name of the category presented in the task.
+		      css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
+		    media : [ //Stimuli content as PIP's media objects
+    		    {image : 'Fat_man_5.jpg'}, 
+    			{image : 'Fat_man_2.jpg'}, 
+    			{image : 'Fat_man_3.jpg'}, 
+    			{image : 'Fat_man_4.jpg'},
+				{image : 'Fat_woman_1.jpg'},
+				{image : 'Fat_woman_2.jpg'}
+		    ], 
+		    //Stimulus css (style)
+		    css : {color:'#0433b4','font-size':'3em'}
+		  },
+
+		  category2 : { 
+		    name : 'Thin People', //Will appear in the data.
+		    title : {
+		      media : {word : 'Thin People'}, //Name of the category presented in the task.
+		      css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
+		    media : [ //Stimuli content as PIP's media objects
+    		    {image : 'Thin_man_1.png'}, 
+    			{image : 'Thin_man_2.png'}, 
+    			{image : 'Thin_man_3.jpeg'}, 
+    			{image : 'Thin_man_4.png'},
+				{image : 'Thin_woman_1.jpeg'},
+				{image : 'Thin_woman_2.png'}
+		    ], 
+		    //Stimulus css (style)
+		    css : {color:'#0433b4','font-size':'3em'}
+		  },
+
+  		attribute1 : 
+			{
+			name : 'Unpleasant', //Attribute label
+			title : {
+				media : {word : 'Unpleasant'}, //Name of the category presented in the task.
+				css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Bomb'},
+				{word: 'Abuse'},
+				{word: 'Sadness'},
+				{word: 'Pain'},
+				{word: 'Poison'},
+				{word: 'Grief'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#0433b4','font-size':'3em'}
+			},
+		attribute2 : 
+			{
+			name : 'Pleasant', //Attribute label
+			title : {
+				media : {word : 'Pleasant'}, //Name of the category presented in the task.
+				css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Paradise'},
+				{word: 'Pleasure'},
+				{word: 'Cheer'},
+				{word: 'Wonderful'},
+				{word: 'Splendid'},
+				{word: 'Love'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#0433b4','font-size':'3em'}
+			},
+
+  base_url : {//Where are your images at?
+	image : 'https://terrifyd.github.io/UD-IAT/Images/'
+    // image : 'https://baranan.github.io/minno-tasks/images/'
+  }}
+  );
+  });
--- Script.js
+++ Script.js
@@ -1,87 +1,108 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/BIAT/qualtrics/qbiat6.js'], function(APIConstructor, iatExtension){
+ var API = new APIConstructor();
+
 	
-	var API = new APIConstructor();
-		  return stiatExtension({
-		  category : { 
-		    name : 'Fat People', //Will appear in the data.
-		    title : {
-		      media : {word : 'Fat People'}, //Name of the category presented in the task.
-		      css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
-		      height : 7 //Used to position the "Or" in the combined block.
-		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    {image : 'Fat_man_5.jpg'}, 
-    			{image : 'Fat_man_2.jpg'}, 
-    			{image : 'Fat_man_3.jpg'}, 
-    			{image : 'Fat_man_4.jpg'},
-				{image : 'Fat_woman_1.jpg'},
-				{image : 'Fat_woman_2.jpg'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#0433b4','font-size':'3em'}
-		  },
-
-		  category2 : { 
-		    name : 'Thin People', //Will appear in the data.
-		    title : {
-		      media : {word : 'Thin People'}, //Name of the category presented in the task.
-		      css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
-		      height : 7 //Used to position the "Or" in the combined block.
-		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    {image : 'Thin_man_1.png'}, 
-    			{image : 'Thin_man_2.png'}, 
-    			{image : 'Thin_man_3.jpeg'}, 
-    			{image : 'Thin_man_4.png'},
-				{image : 'Thin_woman_1.jpeg'},
-				{image : 'Thin_woman_2.png'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#0433b4','font-size':'3em'}
-		  },
-
-  		attribute1 : 
+	return iatExtension({
+		practiceCategory1 : 
 			{
-			name : 'Unpleasant', //Attribute label
-			title : {
-				media : {word : 'Unpleasant'}, //Name of the category presented in the task.
-				css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Bomb'},
-				{word: 'Abuse'},
-				{word: 'Sadness'},
-				{word: 'Pain'},
-				{word: 'Poison'},
-				{word: 'Grief'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#0433b4','font-size':'3em'}
-			},
-		attribute2 : 
+				name : 'Mammals', //Will appear in the data.
+				title : {
+					media : {word : 'Mammals'}, //Name of the category presented in the task.
+					css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+					height : 4, //Height (because we need to know where to put the next item in the title)
+					startStimulus : { 
+					//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
+						media : {word : 'Dogs, Horses, Cows, Lions'}, 
+						css : {color:'#31b404','font-size':'1em'}, 
+						height : 2
+					}
+				}, 
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word : 'Dogs'}, 
+					{word : 'Horses'}, 
+					{word : 'Lions'}, 
+					{word : 'Cows'}
+				], 
+				//Stimulus css (style of the stimuli)
+				stimulusCss : {color:'#31b404','font-size':'2em'}
+			},	
+			practiceCategory2 : 
 			{
-			name : 'Pleasant', //Attribute label
-			title : {
-				media : {word : 'Pleasant'}, //Name of the category presented in the task.
-				css : {color:'#0433b4','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Paradise'},
-				{word: 'Pleasure'},
-				{word: 'Cheer'},
-				{word: 'Wonderful'},
-				{word: 'Splendid'},
-				{word: 'Love'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#0433b4','font-size':'3em'}
+				name : 'Birds', 
+				title : {
+					media : {word : 'Birds'}, 
+					css : {color:'#31b404','font-size':'1.8em'}, 
+					height : 4,
+					startStimulus : {
+						media : {word : 'Pigeons, Swans, Crows, Ravens'}, 
+						css : {color:'#31b404','font-size':'1em'}, 
+						height : 2
+					}
+				}, 
+				stimulusMedia : [ //Stimuli content as PIP's media objects
+					{word : 'Pigeons'}, 
+					{word : 'Swans'}, 
+					{word : 'Crows'}, 
+					{word : 'Ravens'}
+				], 
+				//Stimulus css
+				stimulusCss : {color:'#31b404','font-size':'2em'}
 			},
+			categories : [  //As many categories you need.
+				{
+					name : 'Black People', //Will appear in the data.
+					title : {
+						media : {word : 'Black People'}, //Name of the category presented in the task.
+						css : {color:'#31b404','font-size':'1.8em'}, //Style of the category title.
+						height : 4, //Height (because we need to know where to put the next item in the title)
+						startStimulus : { 
+						//If you're using a startStimulus, set here. If not, set the parameter showStimuliWithInst to false (see later below)
+						media : {image : 'blacks.jpg'}, 
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 13
+						}
+					}, 
+					stimulusMedia : [ //Stimuli content as PIP's media objects
+					{image : 'black1.jpg'}, 
+        			{image : 'black2.jpg'}, 
+        			{image : 'black3.jpg'}, 
+        			{image : 'black4.jpg'}, 
+        			{image : 'black5.jpg'}, 
+        			{image : 'black6.jpg'}
+					], 
+					//Stimulus css (style of the stimuli)
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				},	
+				{
+					name : 'White people', 
+					title : {
+						media : {word : 'White people'}, 
+						css : {color:'#31b404','font-size':'1.8em'}, 
+						height : 4,
+						startStimulus : {
+							media : {image : 'whites.jpg'},  
+							css : {color:'#31b404','font-size':'1em'}, 
+							height : 13
+						}
+					}, 
+					stimulusMedia : [ //Stimuli content as PIP's media objects
+					{image : 'yf1.jpg'}, 
+        			{image : 'yf4.jpg'}, 
+        			{image : 'yf5.jpg'}, 
+        			{image : 'ym2.jpg'}, 
+        			{image : 'ym3.jpg'}, 
+        			{image : 'ym5.jpg'}
+					], 
+					//Stimulus css
+					stimulusCss : {color:'#31b404','font-size':'2em'}
+				}
+			],
+			base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+				
+			} 
+	});
+});
 
-  base_url : {//Where are your images at?
-	image : 'https://terrifyd.github.io/UD-IAT/Images/'
-    // image : 'https://baranan.github.io/minno-tasks/images/'
-  }}
-  );
-  });
+
+
