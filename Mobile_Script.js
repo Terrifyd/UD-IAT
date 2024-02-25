9331eccec2c647b6ab62502aa8dd8bc4fb4ba395
dlturner29@icloud.com <turnerde@udel.edu>
2024-02-24 20:55:08 -0500

    redirect Mobile link

--- Mobile_Script.js
+++ Mobile_Script.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://terrifyd.github.io/UD-IAT/Mobile_Full_Script.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
@@ -41,7 +41,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/'
 		},
-		
+
         isTouch : global.$isTouch
 
 
