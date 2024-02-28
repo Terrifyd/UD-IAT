0abd3b61eb19b7e103bf9d67104647b470b4ebc6
dlturner29@icloud.com <turnerde@udel.edu>
2024-02-27 21:01:53 -0500

    chaged category colors

--- Distractor_Script.js
+++ Distractor_Script.js
@@ -7,7 +7,7 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 			name : 'Insects', //Will appear in the data.
 			title : {
 				media : {word : 'Insects'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				css : {color:'#c04c30','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -19,13 +19,13 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 			{word: 'GNAT'}
 			], 
 			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+			stimulusCss : {color:'#c04c30','font-size':'1.8em'}
 		},	
 		category2 :	{
 			name : 'Flowers', //Will appear in the data.
 			title : {
 				media : {word : 'Flowers'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				css : {color:'#c04c30','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -37,7 +37,7 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 			{word: 'LILY'} 
 			], 
 			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+			stimulusCss : {color:'#c04c30','font-size':'1.8em'}
 		},	
 
 		attribute1 : {
