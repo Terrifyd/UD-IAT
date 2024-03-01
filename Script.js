d730b458587e6845c825b2cc5c65420c31f33e56
dlturner29@icloud.com <turnerde@udel.edu>
2024-03-01 00:29:23 -0500

    fixing image 1 again

--- Script.js
+++ Script.js
@@ -29,7 +29,7 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-			{image : 'Thin_man_1.jpg'}, 
+			{image : 'Thin_man_1.png'}, 
 			{image : 'Thin_man_2.png'}, 
 			{image : 'Thin_man_3.jpeg'}, 
 			{image : 'Thin_man_4.png'}, 
